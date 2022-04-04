8583f9b5ff375be0676d960eb9dc4a8b34a15f9f
Kseniia98 <kseniia.plakhotnia@gmail.com>
2022-04-04 16:37:51 +0300

    Add new buttons

--- index.js
+++ index.js
@@ -7,13 +7,59 @@ const arr = [
 ]
 
 let count = 0;
+image.innerHTML = `<img src="${arr[count]}">`;
+let interval;
 
-setInterval(() => {
+function startSlideshow(){
+
+  interval = setInterval(() => {
+    if(count >= arr.length){
+      count = 0;
+    }
+
+    image.innerHTML = `<img src="${arr[count]}">`;
+    count++;
+  
+  }, 1000)
+}
+
+const startSlide = document.getElementById('startSlide');
+startSlide.addEventListener('click', startSlideshow);
+
+/////////////////
+
+function stopSlideshow(){
+  clearInterval(interval)
+}
+
+const stopSlide = document.getElementById('stopSlide');
+stopSlide.addEventListener('click', stopSlideshow);
+
+/////////////////
+
+const nextSlide = document.getElementById('nextSlide');
+
+function nextSlideshow(){
   if(count >= arr.length){
     count = 0;
   }
-
   image.innerHTML = `<img src="${arr[count]}">`;
   count++;
+}
+nextSlide.addEventListener('click', nextSlideshow);
 
-}, 5000)
+////////////////
+
+const previousSlide = document.getElementById('previousSlide');
+
+function previousSlideshow(){
+  if(count < 0 ){
+    count = arr.length - 1;
+  }
+  image.innerHTML = `<img src="${arr[count]}">`;
+  count--;
+}
+
+previousSlide.addEventListener('click', previousSlideshow);
+
+
